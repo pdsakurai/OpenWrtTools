6d51ddcacfe32de38bb9bf4ad3a90199f97638d0
Paul Mikki Sakurai <23254804+pdsakurai@users.noreply.github.com>
2022-09-12 19:28:31 +0800

    New: restart_dead_wan.sh

---
+++ restart_dead_wan.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+function log() {
+    local tag="restart_dead_wan.sh[$$]"
+    logger -t "$tag" "$@"
+    printf "$tag: $@\n"
+}
+
+readonly ip_version=${1:?Missing: IP version}
+case "$ip_version" in
+	ipv4)
+		interface=wan
+		;;
+	ipv6)
+		interface=wan6
+		;;
+	*)
+		log "Invalid IP version given: $ip_version"
+		;;
+esac
+
+ping -${ip_version#ipv} -c1 -q -w5 -I $interface google.com > /dev/null 2> /dev/null
+if [ $? -ne 0 ]; then
+	log "$interface is down. Trying to recover by restarting it." 
+	ifup $interface
+fi
+
