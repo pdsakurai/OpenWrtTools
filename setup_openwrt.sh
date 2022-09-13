2b2e394d468e509b6d56dd99c4fb8cb8bf7978a7
Paul Mikki Sakurai <23254804+pdsakurai@users.noreply.github.com>
2022-09-13 11:21:55 +0800

    Change: Extracted UCI helper

--- setup_openwrt.sh
+++ setup_openwrt.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 source ./src/logger_helper.sh "setup_openwrt.sh"
+source ./src/uci_helper.sh
 
 UNBOUND_ROOT_DIR="/etc/unbound"
 UNBOUND_CONF_SRV_FULLFILEPATH="$UNBOUND_ROOT_DIR/unbound_srv.conf"
@@ -18,39 +19,6 @@ function install_packages() {
     opkg update
     opkg install ${@:?Missing: packages}
     log "Done installing packages."
-}
-
-function _process_uci_from_file() {
-    local uci_operation="${1:?Missing: UCI operation}"
-    local uci_option="${2:?Missing: UCI option}"
-    local uci_option_values_fullfilepath="${3:?Missing: UCI option values fullfilepath}"
-    local line_cleaner_func="$4"
-
-    [ "$uci_operation" == "add_list" ] && uci -q delete $uci_option
-    while read uci_option_value; do
-        uci_option_value="$( printf "$uci_option_value" | xargs )"
-        [ -n "$line_cleaner_func" ] && uci_option_value="$( $line_cleaner_func "$uci_option_value" )"
-        [ -n "$uci_option_value" ] && case $uci_operation in
-            "add_list")
-                uci add_list $uci_option="$uci_option_value"
-                ;;
-            "set")
-                uci set $uci_option.$uci_option_value
-                ;;
-            *)
-                log "Invalid UCI operation given: $uci_operation"
-                exit 1
-                ;;
-        esac
-    done < "$uci_option_values_fullfilepath"
-}
-
-function set_uci_from_file() {
-    _process_uci_from_file "set" "$1" "$2" "$3"
-}
-
-function add_list_uci_from_file() {
-    _process_uci_from_file "add_list" "$1" "$2" "$3"
 }
 
 function load_and_append_to_another_file() {
---
+++ src/uci_helper.sh
@@ -0,0 +1,32 @@
+function __process_uci_from_file() {
+    local uci_operation="${1:?Missing: UCI operation}"
+    local uci_option="${2:?Missing: UCI option}"
+    local uci_option_values_fullfilepath="${3:?Missing: UCI option values fullfilepath}"
+    local line_cleaner_func="$4"
+
+    [ "$uci_operation" == "add_list" ] && uci -q delete $uci_option
+    while read uci_option_value; do
+        uci_option_value="$( printf "$uci_option_value" | xargs )"
+        [ -n "$line_cleaner_func" ] && uci_option_value="$( $line_cleaner_func "$uci_option_value" )"
+        [ -n "$uci_option_value" ] && case $uci_operation in
+            "add_list")
+                uci add_list $uci_option="$uci_option_value"
+                ;;
+            "set")
+                uci set $uci_option.$uci_option_value
+                ;;
+            *)
+                log "Invalid UCI operation given: $uci_operation"
+                exit 1
+                ;;
+        esac
+    done < "$uci_option_values_fullfilepath"
+}
+
+function set_uci_from_file() {
+    __process_uci_from_file "set" "$1" "$2" "$3"
+}
+
+function add_list_uci_from_file() {
+    __process_uci_from_file "add_list" "$1" "$2" "$3"
+}
