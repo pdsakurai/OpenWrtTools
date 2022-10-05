617a320d16d06a1b4c7fc57ed27936dce045cf9c
Paul Mikki Sakurai <23254804+pdsakurai@users.noreply.github.com>
2022-10-05 19:13:00 +0800

    FIxed: No tag added New: Allow overriding tag

--- src/logger_helper.sh
+++ src/logger_helper.sh
@@ -3,8 +3,22 @@
 __tag="${1:?Missing: Tag}"
 
 function log() {
-    local tag="$__tag[$$]"
-    local text="${@:?Missing: Text to log}"
-    logger "$tag" "$text"
-    printf "$( date -Iseconds ) $tag: $text \n"
+    local tag text time_now="$( date -Iseconds )"
+    case "$#" in
+        1)
+            tag="$__tag"
+            text="$1"
+            ;;
+        2)
+            tag="$1"
+            text="$2"
+            ;;
+        *)
+            echo "Unexpected number of variables: $#"
+            exit 1
+            ;;
+    esac
+    tag="$tag[$$]"
+    logger -t "$tag" "$text"
+    printf "$time_now $tag: $text\n"
 }
