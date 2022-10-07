b58773a8822dff0a8e2421b67e57d41e9c6e6db2
Paul Mikki Sakurai <23254804+pdsakurai@users.noreply.github.com>
2022-10-07 13:25:08 +0800

    Fixed: Cannot setup dawn

--- src/dawn_helper.sh
+++ src/dawn_helper.sh
@@ -8,24 +8,24 @@ __pkg="dawn"
 __resources_dir="${RESOURCES_DIR:?Define ENV var:RESOURCES_DIR}/$__pkg"
 
 function __apply_recommended_uci_options() {
-    install_packages luci-app-$pkg
+    install_packages luci-app-$__pkg
     local broadcast_address="$( ip address | grep inet.*br-lan | sed 's/.*brd \(.*\) scope.*/\1/' )"
 
     function clean_uci_option() {
         printf "$1" | sed s/\$broadcast_address/$broadcast_address/
     }
-    uci revert $pkg
-    set_uci_from_file "$pkg" "$__resources_dir/uci.dawn" "clean_uci_option"
-    commit_and_log_if_there_are_changes "$pkg" "$pkg is now broadcasting via $broadcast_address"
+    uci revert $__pkg
+    set_uci_from_file "$__pkg" "$__resources_dir/uci.dawn" "clean_uci_option"
+    commit_and_log_if_there_are_changes "$__pkg" "$__pkg is now broadcasting via $broadcast_address"
 }
 
 #Reference: https://openwrt.org/docs/guide-user/network/wifi/dawn
 function setup_dawn() {
     $( source $SOURCES_DIR/wireless_helper.sh && __enable_802dot11k_and_802dot11v )
     __apply_recommended_uci_options \
-        && restart_services wpad network $pkg \
+        && restart_services wpad network $__pkg \
         && wifi
-    log "Done setting up $pkg."
+    log "Done setting up $__pkg."
 }
 
 function __remove_uci_options() {
