5abf158723fd97b1397edaff99d38effe545114f
Sakurai, Paul Mikki <23254804+pdsakurai@users.noreply.github.com>
2023-06-29 08:39:25 +0800

    Change: Moved ipv4_subnet.sh to ./src

--- wireguard_helper.sh
+++ wireguard_helper.sh
@@ -18,6 +18,7 @@ readonly SOURCES_DIR="$( pwd )/src"
 export SOURCES_DIR
 source $SOURCES_DIR/temp_file.sh
 source $SOURCES_DIR/timer_helper.sh
+source $SOURCES_DIR/ipv4_subnet.sh
 
 readonly target_uci_section="network.wireguard_$target_interface"
 readonly target_uci_option="$target_uci_section.allowed_ips"
@@ -51,7 +52,7 @@ function reroute_traffic_by_asn() {
     local ip_subnets_file=$( create_temp_file )
     local timer=$( start_timer )
     log_info "Started retrieving IPv4 subnets for ASNs: ${asns// /,}"
-    $( . ./ipv4_subnet.sh; get_ipv4_subnets "$ip_subnets_file" "$asns" )
+    $( get_ipv4_subnets "$ip_subnets_file" "$asns" )
     timer=$( end_timer "$timer" )
     log_info "Done retrieving IPv4 subnets within $timer."
 
