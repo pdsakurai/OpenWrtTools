3a57523848840a367814c911387b41a187e1ea9d
Sakurai, Paul Mikki <23254804+pdsakurai@users.noreply.github.com>
2023-06-29 09:14:17 +0800

    Change: Added logging

--- update_and_upgrade_all_packages.sh
+++ update_and_upgrade_all_packages.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+readonly SOURCES_DIR="$( pwd )/src"
+export SOURCES_DIR
+source $SOURCES_DIR/logger_helper.sh "update_and_upgrade_all_packages.sh"
+
 opkg update
+log "Done updating package lists."
+
 opkg list-upgradable | cut -f 1 -d ' ' | xargs -rt opkg upgrade
+log "Done upgrading packages."
