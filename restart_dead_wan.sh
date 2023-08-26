44b4b50fc79be60575055cc67e59ea02d74e8e90
pdsakurai <23254804+pdsakurai@users.noreply.github.com>
2023-08-26 21:36:45 +0800

    Change: Make it independent from domain resolution

--- restart_dead_wan.sh
+++ restart_dead_wan.sh
@@ -8,16 +8,18 @@ readonly ip_version=${1:?Missing: IP version}
 case "$ip_version" in
 	ipv4)
 		interface=wan
+		target=9.9.9.9
 		;;
 	ipv6)
 		interface=wan6
+		target=2620:fe::fe
 		;;
 	*)
 		log "Invalid IP version given: $ip_version"
 		;;
 esac
 
-ping -${ip_version#ipv} -c1 -q -w50 -I wan google.com &> /dev/null
+ping -${ip_version#ipv} -c1 -q -w50 $target &> /dev/null
 if [ $? -ne 0 ]; then
 	log "$interface is down. Trying to recover by restarting it." 
 	ifup $interface
