7c1f143ab18726969951033b95667366863dae73
Sakurai, Paul Mikki <23254804+pdsakurai@users.noreply.github.com>
2024-02-27 14:55:30 +0800

    Change: Skip updates for wireless-regdb

--- update_and_upgrade_all_packages.sh
+++ update_and_upgrade_all_packages.sh
@@ -7,5 +7,5 @@ source $SOURCES_DIR/logger_helper.sh "update_and_upgrade_all_packages.sh"
 opkg update
 log "Done updating package lists."
 
-opkg list-upgradable | cut -f 1 -d ' ' | xargs -rt opkg upgrade
+opkg list-upgradable | grep -v wireless-regdb | cut -f 1 -d ' ' | xargs -rt opkg upgrade
 log "Done upgrading packages."
